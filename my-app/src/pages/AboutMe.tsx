7ae57d8d30bbc3cb52bff677033cb22b3c50eded
spinosajoseph <spinosajoseph@gmail.com>
2020-05-07 22:50:27 -0400

    add material

---
+++ my-app/src/pages/AboutMe.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AboutMe: React.FC = () => {
+  return <h2>About</h2>;
+};
---
+++ my-app/src/pages/Backboard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Backboard: React.FC = () => {
+  return <h2>Backboard</h2>;
+};
